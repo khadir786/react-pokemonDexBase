be53d1e3e9e374d01c0632a37d8103d8b923d4e7
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-06 22:04:50 +0000

    added barebones register component

---
+++ src/components/Register.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react"
+
+export default function Register() {
+    const [formData, setFormData] = useState(
+        { username: "", password: "" }
+    )
+
+    function handleChange(event) {
+        const { name, value, type, checked } = event.target
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: type === "checkbox" ? checked : value
+            }
+        })
+    }
+    return (
+        <div className="login">
+            <h1>Login</h1>
+            <form>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    onChange={handleChange}
+                    name="username"
+                    value={formData.username}
+                />
+                <input
+                    type="password"
+                    placeholder="Username"
+                    onChange={handleChange}
+                    name="password"
+                    value={formData.password}
+                />
+            </form>
+        </div>
+    )
+}

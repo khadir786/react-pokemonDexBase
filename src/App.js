d22572dfdd3de1a5308dbc4c8b2f46571cff71f3
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-07 17:05:07 +0000

    added transitions

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import Login from './components/Login';
 import Register from './components/Register';
 import Header from './components/Header';
+import { CSSTransition } from 'react-transition-group';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import './css/rl-transitions.css';
 
 function App() {
-  const [showRegister, setShowRegister] = useState(false) 
+  const [showRegister, setShowRegister] = useState(true);
 
   function toggleRegister() {
     setShowRegister(!showRegister);
@@ -15,9 +17,24 @@ function App() {
   return (
     <div className="Landing">
       <Header />
-      {showRegister ? <Login toggleRegister = {toggleRegister}/> : 
-      <Register toggleRegister={toggleRegister}/>}
 
+      <CSSTransition
+        in={!showRegister}
+        timeout={0}
+        classNames="fade"
+        unmountOnExit
+      >
+        <Login toggleRegister={toggleRegister} />
+      </CSSTransition>
+
+      <CSSTransition
+        in={showRegister}
+        timeout={300}
+        classNames="fade"
+        unmountOnExit
+      >
+        <Register toggleRegister={toggleRegister} />
+      </CSSTransition>
     </div>
   );
 }

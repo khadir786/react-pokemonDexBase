86ba94349796f0615e42865f36d53794d82ca6e8
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-07 00:35:00 +0000

    revised App.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Login from './components/Login';
 import Register from './components/Register';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="Landing">
+      <Header />
       <Login />
       <Register />
 

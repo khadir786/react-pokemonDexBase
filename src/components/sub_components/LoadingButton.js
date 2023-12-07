b4e45b33f021e5727788970cb16665e61366d3b6
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-07 16:11:14 +0000

    created loading button

---
+++ src/components/sub_components/LoadingButton.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+
+function LoadingButton() {
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    function simulateNetworkRequest() {
+      return new Promise((resolve) => setTimeout(resolve, 2000));
+    }
+
+    if (isLoading) {
+      simulateNetworkRequest().then(() => {
+        setLoading(false);
+      });
+    }
+  }, [isLoading]);
+
+  const handleClick = () => setLoading(true);
+
+  return (
+    <Button
+      variant="dark"
+      disabled={isLoading}
+      onClick={!isLoading ? handleClick : null}
+    >
+      {isLoading ? 'Loading…' : 'Submit'}
+    </Button>
+  );
+}
+
+export default LoadingButton;

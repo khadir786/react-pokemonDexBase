2d9189063ac12cad8346e1f525391251e59a078f
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-07 16:10:56 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Login from './components/Login';
 import Register from './components/Register';
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {

61352d4527cdc58c393b6bb0f43e3f29f3f03ea6
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-07 00:31:28 +0000

    Added header with logo

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+import logo from '../img/logo.png'
+export default function Header() {
+    return (
+
+        <nav className="header">
+            <img alt="logo" src={logo} />
+        </nav>
+
+    )
+}

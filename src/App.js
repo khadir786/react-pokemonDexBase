7b9404da301e9c6b45217e89855f9939cae595a7
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-08 20:03:03 +0000

    created routes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,25 @@
 import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
-import Header from './components/Header';
-import { CSSTransition } from 'react-transition-group';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import './css/rl-transitions.css';
 
 function App() {
-  const [showRegister, setShowRegister] = useState(true);
-
-  function toggleRegister() {
-    setShowRegister(!showRegister);
-  }
-
   return (
-    <div className="Landing">
-      <Header />
-
-      <CSSTransition
-        in={!showRegister}
-        timeout={0}
-        classNames="fade"
-        unmountOnExit
-      >
-        <Login toggleRegister={toggleRegister} />
-      </CSSTransition>
-
-      <CSSTransition
-        in={showRegister}
-        timeout={300}
-        classNames="fade"
-        unmountOnExit
-      >
-        <Register toggleRegister={toggleRegister} />
-      </CSSTransition>
-    </div>
-  );
+    <Router>
+      <div>
+        {/* Add Navigation here if needed */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          {/* Add other routes here */}
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
 export default App;

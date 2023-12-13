1d426df613c05f86449d2e4461d44a135785f5ea
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-13 16:23:45 +0000

    added partner and trainer name sprite list

---
+++ src/components/data/PartnerSpriteNames.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    "data": {
+        "partner": [
+            {"name": "riolu" },
+            {"name": "eevee" },
+            {"name": "pikachu" },
+            {"name": "oshawott" },
+            {"name": "rotom" }
+        ]
+    }
+}    

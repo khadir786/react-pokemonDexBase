5b3e5043261713a828b8c2805214f6471bbab871
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-14 03:13:49 +0000

    added name and official artwork fields to partner pokemon

--- src/components/data/PartnerSprites.js
+++ src/components/data/PartnerSprites.js
@@ -3,21 +3,41 @@ import oshawott from "../../img/partner/oshawott.gif"
 import pikachu from "../../img/partner/pikachu.gif"
 import riolu from "../../img/partner/riolu.gif"
 import rotom from "../../img/partner/rotom.gif"
-
+import eeveeOfficial from '../../img/partner/official/eevee.png';
+import oshawottOfficial from "../../img/partner/official/oshawott.png"
+import pikachuOfficial from "../../img/partner/official/pikachu.png"
+import rioluOfficial from "../../img/partner/official/riolu.png"
+import rotomOfficial from "../../img/partner/official/rotom.png"
+    
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     "data": {
         "partner": [
-            {"name": "Eevee",
-             "image": eevee },
-            {"name": "Oshawott",
-             "image": oshawott },
-            {"name": "Pikachu",
-             "image": pikachu },
-            {"name": "Riolu",
-             "image": riolu },
-            {"name": "Rotom",
-             "image": rotom }
+            {
+                "name": "Eevee",
+                "image": eevee,
+                "officialImage": eeveeOfficial
+            },
+            {
+                "name": "Oshawott",
+                "image": oshawott,
+                "officialImage": oshawottOfficial
+            },
+            {
+                "name": "Pikachu",
+                "image": pikachu,
+                "officialImage": pikachuOfficial
+            },
+            {
+                "name": "Riolu",
+                "image": riolu,
+                "officialImage": rioluOfficial
+            },
+            {
+                "name": "Rotom",
+                "image": rotom,
+                "officialImage": rotomOfficial
+            }
         ]
     }
 }    

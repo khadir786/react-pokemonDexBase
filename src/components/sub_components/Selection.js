0688d1e2f23ca92d0716d287bad0988798a5dce2
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-16 13:49:51 +0000

    added a typewriter effect to partner dex entry

--- src/components/sub_components/Selection.js
+++ src/components/sub_components/Selection.js
@@ -5,21 +5,48 @@ import Carousel from 'react-bootstrap/Carousel';
 const Selection = ({ images }) => {
   const [activeIndex, setActiveIndex] = useState(2);
   const [activePokemon, setActivePokemon] = useState(null);
+  const [activeEntry, setActiveEntry] = useState("");
   const containerRef = useRef(null);
 
   const handlePreviewClick = (index, pokemon) => {
     setActiveIndex(index);
     setActivePokemon(pokemon.name);
+    setActiveEntry(pokemon.dexEntry);
   };
 
   const handleSelectSlide = (selectedIndex) => {
     console.log('Selected Index:', selectedIndex);
     console.log('Image Name:', images[selectedIndex].name);
-
     setActiveIndex(selectedIndex);
   };
+  
+  const typeWriter = (text) => {
+    let i = 0;
+    const speed = 20;
+    const dexEntryElement = document.querySelector('.dexEntry.active'); // Select the .dexEntry element
 
-  console.log(activePokemon);
+    const type = () => {
+      if (i < text.length) {
+        dexEntryElement.innerHTML += text.charAt(i);
+        i++;
+        setTimeout(type, speed);
+      }
+    };
+
+    type();
+  };
+
+  useEffect(() => {
+    // Clear the dexEntry text when the activeEntry changes
+    document.querySelectorAll('.dexEntry').forEach(element => {
+      element.innerHTML = '';
+      element.classList.remove('active');
+    });
+    
+    const dexEntryElements = document.querySelectorAll('.dexEntry');
+    dexEntryElements[activeIndex].classList.add('active');
+    typeWriter(activeEntry); // Start the animation with the new dexEntry text
+  }, [activePokemon]);
 
   return (
     <div className="Selection" ref={containerRef}>
@@ -37,25 +64,25 @@ const Selection = ({ images }) => {
         </div>
       </div>
       <div className="carousel-container">
-      </div >
-      <Carousel activeIndex={activeIndex}
-        onSelect={handleSelectSlide}
-        interval={null}
-        indicators={false}
-        keyboard={true}
-        controls={false}>
-        {images.map((image, index) => (
-          <Carousel.Item key={index}>
-            <img src={image.officialImage} alt={`${image.name}`} />
-            <Carousel.Caption><div>
-              <h3>{image.name}</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </div>
-            </Carousel.Caption>
-          </Carousel.Item>
-        ))}
-      </Carousel>
-
+        <Carousel
+          activeIndex={activeIndex}
+          onSelect={handleSelectSlide}
+          interval={null}
+          indicators={false}
+          keyboard={true}
+          controls={false}
+        >
+          {images.map((image, index) => (
+            <Carousel.Item key={index}>
+              <img src={image.officialImage} alt={`${image.name}`} />
+              <h3 className='pokemonName'>{image.name}</h3>
+              <div className="dexContainer">
+                <p className={`dexEntry ${index === activeIndex ? 'active' : ''}`}></p>
+              </div>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </div>
     </div>
   );
 };

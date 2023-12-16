700959929235ef1b34e9f29a9cfcdb4b2cc3be1a
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-16 15:35:34 +0000

    added Header to partnerpick

--- src/components/PartnerPick.js
+++ src/components/PartnerPick.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Selection from "./sub_components/Selection";
 import PartnerSprites from "./data/PartnerData.js";
+import Header from "./Header.js"
+import "../css/partner.css"
 
 export default function PartnerPick() {
     const partners = PartnerSprites.data.partner
     return (
-        <div className="partners">
-            <Selection images={partners} />
-        </div>
+        <>
+            <Header />
+            <div className="partners">
+                <Selection images={partners} />
+            </div>
+        </>
     )
 }

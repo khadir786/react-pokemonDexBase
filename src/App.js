4ac884339fd5153d6b70f6b894546baddc104017
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-19 14:33:41 +0000

    removed unused state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';

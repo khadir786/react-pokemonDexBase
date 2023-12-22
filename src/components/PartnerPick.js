e08472d4584b90efcc7be76c8272df7b9c6dc245
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-22 00:20:34 +0000

    revised and added styling to selection and profile

--- src/components/PartnerPick.js
+++ src/components/PartnerPick.js
@@ -1,17 +1,10 @@
 import React from "react";
 import Selection from "./sub_components/Selection";
 import PartnerSprites from "./data/PartnerData.js";
-import Header from "./Header.js"
-import "../css/partner.css"
 
 export default function PartnerPick() {
     const partners = PartnerSprites.data.partner
     return (
-        <>
-            <Header />
-            <div className="partners">
-                <Selection images={partners} />
-            </div>
-        </>
+            <Selection images={partners} />
     )
 }

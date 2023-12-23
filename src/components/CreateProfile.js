3ffe165ffe8d3d24c093b5dc9ea7b62f90c0e3b9
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-23 21:17:48 +0000

    Refactor CSS for Avatar, Selection, and Create Profile pages.

--- src/components/CreateProfile.js
+++ src/components/CreateProfile.js
@@ -1,24 +1,24 @@
 import React from "react";
-import PartnerPick from "./PartnerPick";
-import AvatarPick from "./AvatarPick"
 import Header from "./Header";
-import "../css/create-profile.css"
-
+import AvatarPick from "./AvatarPick";
+import PartnerPick from "./PartnerPick";
+import "../css/create-profile.css";
 
 export default function CreateProfile() {
-    return (
-        <div className="PageContainer">
-            <Header />
-            <h1>Create Your Profile</h1>
-            <div className="CreateProfileContainer">
-                <div className="Profile-AvatarContainer">
-                    <AvatarPick />
-                </div>
-
-                <div className="Profile-PartnerContainer">
-                    <PartnerPick />
-                </div>
-            </div>
+  return (
+    <div className="PageContainer">
+      <Header />
+      <div className="CreateProfileContainer">
+        <h1 className="ProfileTitle">Create Your Profile</h1>
+        <div className="Profile-AvatarContainer">
+          <h2 className="SectionTitle">Select Your Avatar</h2>
+          <AvatarPick />
         </div>
-    )
+        <div className="Profile-PartnerContainer">
+          <h2 className="SectionTitle">Select Your Partner Pokemon</h2>
+          <PartnerPick />
+        </div>
+      </div>
+    </div>
+  );
 }

ef5b14448c536fb4f945fcfa8bbde58792621671
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-23 18:37:21 +0000

    created and updated styles

--- src/components/CreateProfile.js
+++ src/components/CreateProfile.js
@@ -1,13 +1,24 @@
 import React from "react";
 import PartnerPick from "./PartnerPick";
 import AvatarPick from "./AvatarPick"
+import Header from "./Header";
 import "../css/create-profile.css"
+
 
 export default function CreateProfile() {
     return (
-        <div className="CreateProfileContainer">
-            <PartnerPick />
-            <AvatarPick />
+        <div className="PageContainer">
+            <Header />
+            <h1>Create Your Profile</h1>
+            <div className="CreateProfileContainer">
+                <div className="Profile-AvatarContainer">
+                    <AvatarPick />
+                </div>
+
+                <div className="Profile-PartnerContainer">
+                    <PartnerPick />
+                </div>
+            </div>
         </div>
     )
 }

6648920b6e6894030185ef2c3d25ec0fdbc4863b
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-23 22:09:33 +0000

    Update avatar and partner selection UI.

--- src/components/AvatarPick.js
+++ src/components/AvatarPick.js
@@ -6,7 +6,7 @@ import '../css/avatar.css';
 
 export default function TrainerImageGallery() {
   const trainers = TrainerSprites.data.trainers;
-  const itemsPerPage = 30; // Number of items per page
+  const itemsPerPage = 20; // Number of items per page
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedImage, setSelectedImage] = useState(trainers[0].name);
   const [selectedModalImage, setSelectedModalImage] = useState(null);
--- src/components/CreateProfile.js
+++ src/components/CreateProfile.js
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import AvatarPick from "./AvatarPick";
 import PartnerPick from "./PartnerPick";
+import Carousel from 'react-bootstrap/Carousel';
+
 import "../css/create-profile.css";
+import "../css/carousel.css";
 
 export default function CreateProfile() {
-  return (
-    <div className="PageContainer">
-      <Header />
-      <div className="CreateProfileContainer">
-        <h1 className="ProfileTitle">Create Your Profile</h1>
-        <div className="Profile-AvatarContainer">
-          <h2 className="SectionTitle">Select Your Avatar</h2>
-          <AvatarPick />
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const handleSelectSlide = (selectedIndex) => {
+        console.log('Selected Index:', selectedIndex);
+        setActiveIndex(selectedIndex);
+    };
+
+    return (
+        <div className="PageContainer">
+            <Header />
+            <div className="CreateProfileContainer">
+                <h1 className="ProfileTitle">Create Your Profile</h1>
+                <div className="carousel-container">
+                    <Carousel
+                        activeIndex={activeIndex}
+                        onSelect={handleSelectSlide}
+                        interval={null}
+                        indicators={true}
+                        keyboard={true}
+                        controls={false}
+                    >
+                        <Carousel.Item key={0}>
+                            <div className="Profile-AvatarContainer">
+                                <h2 className="SectionTitle">Select Your Avatar</h2>
+                                <AvatarPick />
+                            </div>
+                        </Carousel.Item>
+                        <Carousel.Item key={1}>
+                            <div className="Profile-PartnerContainer">
+                                <h2 className="SectionTitle">Select Your Partner Pokemon</h2>
+                                <PartnerPick />
+                            </div>
+                        </Carousel.Item>
+                    </Carousel>
+                </div>
+            </div>
         </div>
-        <div className="Profile-PartnerContainer">
-          <h2 className="SectionTitle">Select Your Partner Pokemon</h2>
-          <PartnerPick />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }

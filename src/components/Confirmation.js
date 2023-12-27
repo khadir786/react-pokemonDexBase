2e7b781044e8bbe7b7a6ad726d1419636ac7177c
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-27 04:28:59 +0000

    added confirmation sections and styles

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,20 +1,41 @@
 import React from "react";
 
-export default function Confirmation( {userData, username, activeIndex, setActiveIndex} ) {
+export default function Confirmation({ userData, username, activeIndex, setActiveIndex }) {
 
 
-    const avatar = userData.avatar;
+  const avatar = userData.avatar;
+  console.log(userData);
 
-    return (
-        <div className="Confirmation-Container">
-            <div className="selected-avatar">
+  return (
+    <div className="Confirmation-Container">
+      <div className="selected-avatar">
         <img
           src={`https://play.pokemonshowdown.com/sprites/trainers/${avatar}.png`}
           alt={avatar}
           onClick={() => (setActiveIndex(0))}
         />
       </div>
-      <h1>{username}</h1>   
-        </div>
-    )
+      <div className="Confirmation-username">
+        <h1>{username}</h1>
+      </div>
+      <div className="Confirmation-spriteContainer">
+        <img
+          src={userData.partnerPokemon?.image}
+          alt={userData.partnerPokemon?.name}
+          onClick={() => (setActiveIndex(1))}
+        />
+      </div>
+      <>
+        <p>{userData.partnerPokemon?.name}</p>
+      </>
+      <div className="Confirmation-officialImageContainer">
+        <img
+          className="Confirmation-officialImage"
+          src={userData.partnerPokemon?.officialImage}
+          alt={userData.partnerPokemon?.name}
+          onClick={() => (setActiveIndex(1))}
+        />
+      </div>
+    </div>
+  )
 }

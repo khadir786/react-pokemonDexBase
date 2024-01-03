40734e7fa7a3013016961d29bc16d94dd0a90523
Khadir Abdul <khadir786.ka@googlemail.com>
2024-01-03 00:09:20 +0000

    implement LoginRedirect component and UserProvider from UserContext

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { UserProvider } from './components/UseContext';
 import Home from './components/Home';
 import Login from './components/Login';
+import LoginRedirect from './components/sub_components/LoginRedirect';
 import ProtectedRoute from './components/sub_components/ProtectedRoute'
 import Register from './components/Register';
 import HomeUser from './components/HomeUser';
@@ -29,49 +31,64 @@ function App() {
   console.log("Logged in: " + isLoggedIn);
 
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
-          <Route path="/register" element={<Register isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
-          <Route path="/login" element={<Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
+    <UserProvider>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={
+              <LoginRedirect isLoggedIn={isLoggedIn}>
+                <Home isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+              </LoginRedirect>
+            } />
+            <Route path="/login" element={
+              <LoginRedirect isLoggedIn={isLoggedIn}>
+                <Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+              </LoginRedirect>
+            } />
+            <Route path="/register" element={
+              <LoginRedirect isLoggedIn={isLoggedIn}>
+                <Register isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+              </LoginRedirect>
+            } />
 
-          {/* Protected Routes */}
-          <Route
-            path="/home"
-            element={
-              <ProtectedRoute isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}>
-                <HomeUser />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/trainer"
-            element={
-              <ProtectedRoute isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}>
-                <TrainerImageGallery />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/partner"
-            element={
-              <ProtectedRoute isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}>
-                <PartnerPick />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/create-profile"
-            element={
-              <ProtectedRoute isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}>
-                <CreateProfile />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </div>
-    </Router>
+            {/* Protected Routes */}
+            <Route
+              path="/home"
+              element={
+                <ProtectedRoute isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}>
+                  <HomeUser />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/trainer"
+              element={
+                <ProtectedRoute isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}>
+                  <TrainerImageGallery />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/partner"
+              element={
+                <ProtectedRoute isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}>
+                  <PartnerPick />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/create-profile"
+              element={
+                <ProtectedRoute isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}>
+                  <CreateProfile />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
+    </UserProvider>
+
   );
 }
 

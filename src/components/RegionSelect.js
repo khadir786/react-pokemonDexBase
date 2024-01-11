a4bb56c64d6e95208adad97fe42a4ad544804f21
Khadir Abdul <khadir786.ka@googlemail.com>
2024-01-11 02:38:41 +0000

    created RegionSelect component

--- src/components/RegionSelect.js
+++ src/components/RegionSelect.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import Nav from 'react-bootstrap/Nav';
+import Regions from "./data/RegionData"
 
+import '../css/region.css'
+
+const regions = Regions.data.regions;
 
 export default function RegionSelect({ userData, setUserData }) {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [selectedRegion, setSelectedRegion] = useState(null);
+
+    console.log(selectedRegion)
+
+    const handleSelectSlide = (selectedIndex) => {
+        console.log('Selected Index:', selectedIndex);
+        console.log('Image Name:', regions);
+        setActiveIndex(selectedIndex);
+        setSelectedRegion(regions[selectedIndex].name);
+        setUserData(prevUserData => ({
+            ...prevUserData, 
+            region: regions[selectedIndex].name,
+        }))
+    };
+
     return (
         <div className="Region-Select">
+            <div className='Region-Nav'>
+                <Nav fill variant="tabs">
+                    {regions.map((region, index) => (
+                        <Nav.Item key={index}>
+                            <Nav.Link onClick={() => (handleSelectSlide(index))}>{region.name}</Nav.Link>
+                        </Nav.Item>
+                    ))}
+                </Nav>
+            </div>
+
+            <Carousel
+                activeIndex={activeIndex}
+                onSelect={handleSelectSlide}
+                interval={null}
+                indicators={false}
+                keyboard={true}
+            >
+                {regions.map((region, index) => (
+                    <Carousel.Item key={index}>
+                        <img className="Region-Image" key={index} src={region.image} alt={`${index}`} />
+                    </Carousel.Item>
+                ))}
+            </Carousel>
             <p>region select</p>
+
         </div>
     )
 }

d4b34dae3d184768506c3c1adeb7c7592a3017c9
Khadir Abdul <khadir786.ka@googlemail.com>
2024-01-14 17:57:21 +0000

    Updated UI for header and profile picture

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import React from "react"
 import logo from '../img/logo.png'
 import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Overlay from 'react-bootstrap/Overlay';
+import Tooltip from 'react-bootstrap/Tooltip';
 import { logoutUser } from "../apiService";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "./UseContext";
@@ -23,12 +26,25 @@ export default function Header({ isLoggedIn, setIsLoggedIn, logged, userData }) 
             })
     };
 
+    console.log("avatar for profile pic: ", userData.avatar);
     return (
+
         <nav style={{ width: '100%' }} className={isLoggedIn ? "headerLogged" : "header"}>
             <img className="Logo" alt="logo" src={logo} />
-            {isLoggedIn &&
-                <Button variant="secondary" className="LogoutButton" onClick={handleLogout}>Logout</Button>}
+            {
+                isLoggedIn &&
+                <div className="header-right">
+                    {userData &&
+                        <div className="profile-pic-Container">
+                            <img className="profile-pic" src={`https://play.pokemonshowdown.com/sprites/trainers/${userData.avatar}.png`} alt="profile pic" />
+                        </div>
+                    }
+                    <Button variant="secondary" className="LogoutButton" onClick={handleLogout}>Logout</Button>
+                </div>
+            }
+
         </nav>
+
 
     )
 }

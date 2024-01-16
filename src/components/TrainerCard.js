efe3817defa7ba536d5dcff453e81ac0dc500b91
Khadir Abdul <khadir786.ka@googlemail.com>
2024-01-16 14:35:09 +0000

    Updated Trainer Card to include user's ID number

--- src/components/TrainerCard.js
+++ src/components/TrainerCard.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useUser } from "./UseContext.js";
+
 import '../css/trainer-card.css';
 
 const TrainerCard = ({ username, avatar, dob, region }) => {
   
+  const { user } = useUser();
+
   function calculateAge(dob) {
     var ageDifMs = Date.now() - dob.getTime();
     var ageDate = new Date(ageDifMs); // miliseconds from epoch
@@ -16,7 +20,8 @@ const TrainerCard = ({ username, avatar, dob, region }) => {
     <div className="trainer-card">
       <img className="trainer-image" src={`https://play.pokemonshowdown.com/sprites/trainers/${avatar}.png`} alt={username} />
       <div className="trainer-info">
-        <h2 className="trainer-name">{username}</h2>
+        <h2 className="trainer-id">ID: {user.id}</h2>
+        <h2 className="trainer-name">Name: {username}</h2>
         <p className="trainer-age">Age: {age}</p>
         <p className="trainer-location">Location: {region}</p>
       </div>

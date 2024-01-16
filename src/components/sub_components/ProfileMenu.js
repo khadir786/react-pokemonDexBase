7c3a72ab00f8f2e3b79d80290dc8f8b54791113c
Khadir Abdul <khadir786.ka@googlemail.com>
2024-01-16 00:43:32 +0000

    added edit profile section to menu

--- src/components/sub_components/ProfileMenu.js
+++ src/components/sub_components/ProfileMenu.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Popover from 'react-bootstrap/Popover';
-
+import { useNavigate } from 'react-router-dom';
 
 export default function ProfileMenu({ userData, handleLogout }) {
+    const navigate = useNavigate()
+
 
     return (
         <div className="profile-pic-Container">
@@ -15,7 +17,7 @@ export default function ProfileMenu({ userData, handleLogout }) {
                     <Popover id={`popover-positioned-bottom`}>
                         <Popover.Body style={{ padding: '5px' }}>
                             <ListGroup variant="flush">
-                                <ListGroup.Item eventKey={1} action className="menu-items">Edit Profile</ListGroup.Item>
+                                <ListGroup.Item eventKey={1} action onClick={() => {navigate("/profile")}}className="menu-items">Edit Profile</ListGroup.Item>
                                 <ListGroup.Item eventKey={2} action onClick={handleLogout} className="menu-items">Logout</ListGroup.Item>
                             </ListGroup>
                         </Popover.Body>
